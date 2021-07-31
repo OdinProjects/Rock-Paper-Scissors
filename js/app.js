858cebfce8da81e224a3cc95553c31e63f3a2c3c
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-07-31 16:21:35 +0700

    Added response for every clicked button

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 import {computerPlay} from './rpsGenerator.js'
 import {playOneRound} from './playOneRound.js'
 
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        alert(button.id)
+    })
+})
+
+/*
 const game = () => {
     for(let i = 0; i < 4; i++) {
         const userChoice = prompt("Choose between rock or paper or scissors")
@@ -10,3 +19,4 @@ const game = () => {
 }
 
 game()
+*/
