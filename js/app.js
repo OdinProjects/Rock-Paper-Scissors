548ce28cd6c8657e71ceea34f7bab72b1275fee8
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-07-31 08:57:36 +0700

    Play the game 5 rounds

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 import {computerPlay} from './rpsGenerator.js'
+import {playOneRound} from './playOneRound.js'
 
-console.log(computerPlay())
+const game = () => {
+    for(let i = 0; i < 4; i++) {
+        const userChoice = prompt("Choose between rock or paper or scissors")
+        const result = playOneRound(userChoice, computerPlay())
+        alert(result)
+    }
+}
+
+game()
