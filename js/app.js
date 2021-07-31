be5b12b74900c4a8c4cbdd502328399983f6160f
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-07-31 17:15:19 +0700

    Showed each round's result on the screen

--- js/app.js
+++ js/app.js
@@ -1,24 +1,15 @@
 import {computerPlay} from './rpsGenerator.js'
 import {playOneRound} from './playOneRound.js'
 
-const game = (event) => {
-    alert(event.target.id);
-}
-
 const buttons = document.querySelectorAll('button');
+const result = document.querySelector('#result')
+
+const game = (event) => {
+    const message = document.createElement('p')
+    message.textContent = playOneRound(event.target.id, computerPlay())
+    result.appendChild(message)
+}
 
 buttons.forEach(button => {
     button.addEventListener('click', game)
 })
-
-/*
-const game = () => {
-    for(let i = 0; i < 4; i++) {
-        const userChoice = prompt("Choose between rock or paper or scissors")
-        const result = playOneRound(userChoice, computerPlay())
-        alert(result)
-    }
-}
-
-game()
-*/
