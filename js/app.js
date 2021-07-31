db708e0e0494edd29a508cdcffd75c32aa3c75ff
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-07-31 18:23:47 +0700

    Count until 5 points and determine the winner

--- js/app.js
+++ js/app.js
@@ -2,14 +2,35 @@ import {computerPlay} from './rpsGenerator.js'
 import {playOneRound} from './playOneRound.js'
 
 const buttons = document.querySelectorAll('button');
-const result = document.querySelector('#result')
+const result = document.querySelector('#result');
+
+let currentPlayerScore = 0;
+let currentOpponentScore = 0;
 
 const game = (event) => {
-    const message = document.createElement('p')
-    message.textContent = playOneRound(event.target.id, computerPlay())
-    result.appendChild(message)
+    const message = document.createElement('p');
+    const play = playOneRound(event.target.id, computerPlay(), currentPlayerScore, currentOpponentScore);
+    message.textContent = play[0];
+    currentPlayerScore = play[1];
+    currentOpponentScore = play[2];
+    console.log(currentPlayerScore, currentOpponentScore)
+    result.appendChild(message);
+
+    const winner = document.createElement('p');
+    if(currentPlayerScore === 5) {
+        winner.textContent = "Congrats! You've just won the game!";
+        currentPlayerScore = 0;
+        currentOpponentScore = 0;
+        result.appendChild(winner);
+    }
+    else if(currentOpponentScore === 5) {
+        winner.textContent = "Better Luck Next Time!";
+        currentPlayerScore = 0;
+        currentOpponentScore = 0;
+        result.appendChild(winner);
+    }   
 }
 
 buttons.forEach(button => {
-    button.addEventListener('click', game)
+    button.addEventListener('click', game);
 })
